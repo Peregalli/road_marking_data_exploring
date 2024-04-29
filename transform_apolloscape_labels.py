021e7cf9f0c48ed8fde94eca8ff1e5b1a483cd5d
Peregalli <adiazperegalli@gmail.com>
2024-04-29 12:53:06 -0300

    Add a config/config.json file with dataset informacion

--- transform_apolloscape_labels.py
+++ transform_apolloscape_labels.py
@@ -7,8 +7,8 @@ import pandas as pd
 import random
 
 from tqdm import tqdm
-from image_utils import load_labels_from_json, get_labels_from_mask, preprocessing_blocks
-from utils import list_to_csv
+from image_utils import get_labels_from_mask, preprocessing_blocks
+from utils import list_to_csv, load_from_json
 
 
 def change_mask_label(mask: np.ndarray,
@@ -84,14 +84,14 @@ def get_records_names_from_road(road: int, root: str) -> list:
 
 def transform_apolloscape_labels(road: int,
                                  records: list,
-                                 camara: int,
                                  root_to_data: str,
                                  dst_folder):
 
-    dataset_v1 = pd.read_csv('apolloscape_labels_v1.csv')
+    dataset_v1 = pd.read_csv('config/apolloscape_labels_v1.csv')
     dataset_v1['color'] = dataset_v1['color'].apply(safe_literal_eval)
     dataset_v1['new_label'] = dataset_v1['new_label'].apply(safe_literal_eval)
-    old_labels = load_labels_from_json('dataset_labels.json')['apolloscape_lanemark']['labels']
+    old_labels = load_from_json('config/dataset_labels.json')['apolloscape_lanemark']['labels']
+    cfg = load_from_json('config/config.json')
     # Create folders
     dst_folder_img = os.path.join(dst_folder, 'images')
     dst_folder_mask = os.path.join(dst_folder, 'masks')
@@ -99,26 +99,31 @@ def transform_apolloscape_labels(road: int,
     create_folder(dst_folder_img)
 
     for record in records:
-        print(f'Road {road:02d} - '+record+' - Camera {camara:d}')
-        folder_to_labels = os.path.join(root_to_data, f'Labels_road{road:02d}/Label/{record}/Camera {camara:d}')
-        folder_to_images = os.path.join(root_to_data, f'ColorImage_road{road:02d}/ColorImage/{record}/Camera {camara:d}')
+        print(f'Road {road:02d} - '+record+f" - Camera {cfg['camera']:d}")
+        folder_to_labels = os.path.join(root_to_data, f'Labels_road{road:02d}/Label/{record}/Camera {cfg["camera"]:d}')
+        folder_to_images = os.path.join(root_to_data, f'ColorImage_road{road:02d}/ColorImage/{record}/Camera {cfg["camera"]:d}')
         fn_mask = sorted(os.listdir(folder_to_labels))
         new_dataset = init_dataframe(dataset_v1)
-        dst_fn = f'labels_road{road:02d}_{record}_camera{camara:d}.csv'
+        dst_fn = f'labels_road{road:02d}_{record}_camera{cfg["camera"]:d}.csv'
 
         for fn in tqdm(fn_mask):
             img_fn = fn.replace('_bin.png', '.jpg')
             img = cv.imread(os.path.join(folder_to_images, img_fn))
             mask = cv.imread(os.path.join(folder_to_labels, fn))
-            block_imgs, block_masks = preprocessing_blocks(img, mask, 512, 0)
+            block_imgs, block_masks = preprocessing_blocks(img,
+                                                           mask,
+                                                           cfg["cell_size"],
+                                                           cfg["overlap"])
             # Preprocess data
             for i, block_mask in enumerate(block_masks):
                 lables_used = get_labels_from_mask(block_mask, old_labels)
                 new_mask = block_mask[:, :, ::-1].copy()
                 for label_used in lables_used:
-                    new_mask = change_mask_label(new_mask, label_used, dataset_v1)
+                    new_mask = change_mask_label(new_mask,
+                                                 label_used,
+                                                 dataset_v1)
                 new_fn_mask = fn.replace('_bin.png', f'_{i}_bin.png')
-                new_fn_img = img_fn.replace('.png', f'_{i}.png')
+                new_fn_img = img_fn.replace('.jpg', f'_{i}.jpg')
                 new_dataset = upload_dataframe(dataset_v1,
                                                new_dataset,
                                                new_fn_mask,
@@ -142,13 +147,11 @@ def main():
 
     args = parser.parse_args()
 
-    roads = [2, 3, 4]
+    roads = [3, 4]
     total_records = [get_records_names_from_road(road, args.root_to_data) for road in roads]
-    camara = 6
     for i, road in enumerate(roads):
         transform_apolloscape_labels(road,
                                      total_records[i],
-                                     camara,
                                      args.root_to_data,
                                      args.root_dst)
     split_sets(args.root_dst)
